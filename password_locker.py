491fc984ec6fa5711b8c50aa560c2cde67a829f6
Beryl Onyancha <berylonyancha@gmail>
2019-06-14 15:16:35 +0300

    Add function to delete users

--- password_locker.py
+++ password_locker.py
@@ -10,15 +10,14 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.password = password 
-    def __save_user__(self):
+    def save_user(self):
         '''
         function to save user
         '''    
-
-
-
-
-
-
-
+        User.user_list.append(self)
+    def delete_user(self):
+        '''
+        function to delete user
+        '''
+        User.user_list.remove(self)
 class Credentials:  
