6360debbe96462316a91b61054aa516adbff30de
Beryl Onyancha <berylonyancha@gmail>
2019-06-16 20:50:48 +0300

    Create set up method to run before each test cases

--- password_locker_test.py
+++ password_locker_test.py
@@ -1,2 +1,6 @@
 import unittest
 from password_locker import User, Credentials
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User("Beryl","Onyancha","skittles")
