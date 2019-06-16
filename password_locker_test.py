a6518b9e2f36d266447931a0c61c172f774ef2de
Beryl Onyancha <berylonyancha@gmail>
2019-06-16 21:05:09 +0300

    Create tear down method

--- password_locker_test.py
+++ password_locker_test.py
@@ -3,4 +3,12 @@ from password_locker import User, Credentials
 
 class TestUser(unittest.TestCase):
     def setUp(self):
+        '''
+        Set up method to run before  each test
+        '''
         self.new_user = User("Beryl","Onyancha","skittles")
+    def tearDown(self):
+        '''
+        Tear down method performs clean up after each test method completes
+        '''
+        User.user_list = []
