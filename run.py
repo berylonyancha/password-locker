dea023c736b23215554d70910ed8a972385302bb
Beryl Onyancha <berylonyancha@gmail>
2019-06-17 11:25:26 +0300

    Add more information to the main function

--- run.py
+++ run.py
@@ -39,8 +39,46 @@ def main():
     print(f"Hello {user_name}. How may I be of service to you?")
     print('\n')
     while True:
-        print("Use these short codes : ca-Create a new user,dc-Display credentials,fs-Find a site, ex-Exit ")
+        print("Use these short codes : cc-Create a new credentials,dc-Display credentials,fs-Find a site, ex-Exit ")
         short_code = input().lower()
+        if short_code == 'cc':
+            print("New Credentials")
+            print("-"*10)
+            print("Site Name...")
+            site = input()
+            print("User name...")
+            user_name = input()
+            print("Enter password for the account")
+            password = input()
+            save_password(create_credential(site,user_name,password))
+            print('\n')
+            print(f"New site {site} for {user_name} created")
+            print('\n')
+      elif short_code == 'dc':
+          if display_passwords():
+              print("Here is a list of saved credentials")
+              print('\n')
+              for credentials in display_passwords():
+                  print(f"{credentials.site}{credentials.user_name}.....{credentials.password}")
+                  print('\n')
+              else:
+                  print('\n')
+                  print("You dont have anything saved")
+                  print('\n')
+      elif short_code == 'fs':
+          print("Enter the site you want to search for")
+          search_site = input()
+          if find_site(search_site):
+              search_site = find_site(search_site)
+              print(f"{search_site.site}{search_site.user_name}{search_site.password}")
+          else:
+              print("That site does not exist")
+      elif short_code == 'ex':
+          print(:"Bye Kwaheri")
+          break
+      else:
+          print("Oops wrong input. Please use shirtg codes provided")
+
 if __name__ == '__main__':
 
     main()
